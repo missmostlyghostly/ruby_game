2aaf5276caf8c0aa4bf6ce21d62c245e775d8203
Will <missmostlyghostly@gmail.com>
2021-04-08 15:31:41 -0700

    Beginnings

---
+++ intro.rb
@@ -0,0 +1,6 @@
+def introText
+    puts "Hello There"
+    puts "What is your name?"
+    playerName = gets.chomp
+    puts "Hello, #{playerName}."
+end
---
+++ main.rb
@@ -0,0 +1,3 @@
+require 'intro.rb'
+
+introText
